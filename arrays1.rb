1d1df4124f21c0da882b2add685e966208594f26
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-01 22:42:31 -0400

    nesting

--- arrays1.rb
+++ arrays1.rb
@@ -1,16 +1,19 @@
-players = [
-    [27, 'Jose Altuve'],
-    [2, 'Alex Bregman'],
-    [1, 'Carlos Correa'],
-    [9, 'Marwin Gonzalez'],
-    [10, 'Yulieski Gurriel']
+nest = [
+    [200, 300, 1000, 'Edgar'],
+    [2, -3040, -29, 'Pato'],
+    [1, -2, 20, 'Nestor'],
+    [9, 2, 40, 'Graciela'],
+    [10, 1500, 90, 'Lorena'],
+    [43040, 605060, 2, 'Gabriel'],
+    [828, -40504, 982, 'Dante'],
+    [928, 2, 0, 'Fabiola']
 ]
 
 def find_element collection, element
     collection.each do |c|
-        puts c if c[0] == element
+        puts c if c[3] == element
     end
 end
 
-find_element(players, 10)
+find_element(nest, 'Fabiola')
 
