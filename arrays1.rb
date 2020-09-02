d2cde63bd5f6970d7a2c3ba41f6fc390792eb9c2
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-01 22:50:01 -0400

    finding elements from a nested array

--- arrays1.rb
+++ arrays1.rb
@@ -1,19 +1,29 @@
 nest = [
-    [200, 300, 1000, 'Edgar'],
-    [2, -3040, -29, 'Pato'],
-    [1, -2, 20, 'Nestor'],
-    [9, 2, 40, 'Graciela'],
-    [10, 1500, 90, 'Lorena'],
-    [43040, 605060, 2, 'Gabriel'],
-    [828, -40504, 982, 'Dante'],
-    [928, 2, 0, 'Fabiola']
-]
+        [200, 300, 1000, 'Edgar'],
+        [2, -3040, -29, 'Pato'],
+        [9, 2, 40, 'Graciela'],
+        [10, 1500, 90, 'Lorena'],
+        [43040, 605060, 2, 'Gabriel'],
+        [828, -40504, 982, 'Dante'],
+        [928, 2, 0, 'Fabiola'],
+        [1, -2, 20, 'Nestor']
+    ],
+    [
+        [2, -3040, -29, 'Andrew'],
+        [1, -2, 20, 'John'],
+        [9, 2, 40, 'Laura'],
+        [10, 1500, 90, 'Nicky'],
+        [43040, 605060, 2, 'Kyle'],
+        [828, -40504, 982, 'Jen'],
+        [928, 2, 0, 'Olivia'],
+        [200, 300, 1000, 'Grace']
+    ]
 
-def find_element collection, element
-    collection.each do |c|
-        puts c if c[3] == element
+def find_element ( collection, element )
+    collection.each do | c |
+        puts c if c[ 0 ] >= element
     end
 end
 
-find_element(nest, 'Fabiola')
+find_element( nest[1], 500 )
 
